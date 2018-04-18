42eb74db9afe45b3321559e41fea9ddd9ee78793
Stalex89 <alex@example.com>
2018-04-18 17:04:26 +0200

    add user login

---
+++ app/controllers/Secured.java
@@ -0,0 +1,75 @@
+package controllers;
+
+import models.UserInfo;
+import models.UserInfoDB;
+import play.mvc.Result;
+import play.mvc.Security;
+import play.mvc.Http.Context;
+
+/**
+ * Implement authorization for this system.
+ * getUserName() and onUnauthorized override superclass methods to restrict
+ * access to the profile() page to logged in users.  
+ * 
+ * getUser(), isLoggedIn(), and getUserInfo() provide static helper methods so that controllers
+ * can know if there is a logged in user.
+ * 
+ * @author Philip Johnson
+ */
+public class Secured extends Security.Authenticator {
+
+  // public Secured() {
+  //   super();
+  //   UserInfoDB.addUserInfo("admin", "admin@example.com", "password");
+  // }
+
+
+
+  /**
+   * Used by authentication annotation to determine if user is logged in.
+   * @param ctx The context.
+   * @return The email address of the logged in user, or null if not logged in.
+   */
+  @Override
+  public String getUsername(Context ctx) {
+    return ctx.session().get("email");
+  }
+
+  /**
+   * Instruct authenticator to automatically redirect to login page if unauthorized.
+   * @param context The context.
+   * @return The login page.
+   */
+  @Override
+  public Result onUnauthorized(Context context) {
+    return redirect(routes.PlaceController.login()); 
+  }
+  
+  /**
+   * Return the email of the logged in user, or null if no logged in user.
+   * 
+   * @param ctx the context containing the session
+   * @return The email of the logged in user, or null if user is not logged in.
+   */
+  public static String getUser(Context ctx) {
+    return ctx.session().get("email");
+  }
+  
+  /**
+   * True if there is a logged in user, false otherwise.
+   * @param ctx The context.
+   * @return True if user is logged in.
+   */
+  public static boolean isLoggedIn(Context ctx) {
+    return (getUser(ctx) != null);
+  }
+  
+  /**
+   * Return the UserInfo of the logged in user, or null if no user is logged in.
+   * @param ctx The context.
+   * @return The UserInfo, or null.
+   */
+  public static UserInfo getUserInfo(Context ctx) {
+    return (isLoggedIn(ctx) ? UserInfoDB.getUser(getUser(ctx)) : null);
+  }
+}
---
+++ app/models/UserInfoDB.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Provides an in-memory repository for UserInfo.
+ * Storing credentials in the clear is kind of bogus.
+ * @author Philip Johnson
+ */
+public class UserInfoDB {
+  private static Map<String, UserInfo> userinfos = new HashMap<String, UserInfo>();
+  
+  /**
+   * Adds the specified user to the UserInfoDB.
+   * @param name Their name.
+   * @param email Their email.
+   * @param password Their password. 
+   */
+  public static void addUserInfo(String name, String email, String password) {
+    userinfos.put(email, new UserInfo(name, email, password));
+  }
+  
+  /**
+   * Returns true if the email represents a known user.
+   * @param email The email.
+   * @return True if known user.
+   */
+  public static boolean isUser(String email) {
+    return userinfos.containsKey(email);
+  }
+
+  /**
+   * Returns the UserInfo associated with the email, or null if not found.
+   * @param email The email.
+   * @return The UserInfo.
+   */
+  public static UserInfo getUser(String email) {
+    return userinfos.get((email == null) ? "" : email);
+  }
+
+  /**
+   * Returns true if email and password are valid credentials.
+   * @param email The email. 
+   * @param password The password. 
+   * @return True if email is a valid user email and password is valid for that email.
+   */
+  public static boolean isValid(String email, String password) {
+    return ((email != null) 
+            &&
+            (password != null) 
+            &&
+            isUser(email) 
+            &&
+            getUser(email).getPassword().equals(password));
+  }
+}
